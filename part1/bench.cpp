65c377a040a3d4310cd64ef5da86f8edd4444c51
Spencer LaChance <spencerlachance25@gmail.com>
2020-02-19 14:03:14 -0500

    Added a separate file to generate datafile and added zip back to git

--- part1/bench.cpp
+++ part1/bench.cpp
@@ -236,7 +236,7 @@ void pmap_example_2(DataFrame* df) {
     delete ir;
 }
 
-main(int argc, char** argv) {
+int main(int argc, char** argv) {
     ParserMain* pf = new ParserMain(argc, argv);
     DataFrame* df = new DataFrame(*(pf->get_dataframe()));
     Sys sys;
@@ -261,12 +261,4 @@ main(int argc, char** argv) {
     }
     delete df;
     return 0;
-
-    // FILE* file = fopen("datafile.txt", "w");
-
-    // for (int i = 0; i < 866666; i++) {
-    //     fputs("<+1><0><1.0><10><1><7.7><88><0>\n", file);
-    //     fputs("<2><0><6767.239302><10990><0><2344.9><7232><1>\n", file);
-    //     fputs("<3><1><11.11><666><0><234.0000001><7890><1>\n", file);
-    // }
 }
